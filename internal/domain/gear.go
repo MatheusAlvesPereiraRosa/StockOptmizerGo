5ebb6d8227bf5d62f88e6be6b7bcbf5e36aa926f
MatheusAlvesPereiraRosa <matpr.a98@gmail.com>
2026-06-28 18:18:50 -0300

    update: creating MongoGearRepository implemenation, initializer and methods for the interface

--- internal/domain/gear.go
+++ internal/domain/gear.go
@@ -3,13 +3,13 @@ package domain
 import "github.com/google/uuid"
 
 type Gear struct {
-	id                uuid.UUID `json:"id" bson:"_id"`
-	name              string    `json:"name" bson:"name"`
-	category          string    `json:"category" bson:"category"`
-	currentStock      int       `json:"currentStock" bson:"currentStock"`
-	minimumStock      int       `json:"minimumStock" bson:"minimumStock"`
-	averageDailySales float64   `json:"averageDailySales" bson:"averageDailySales"`
-	leadTimeInDays    int       `json:"leadTimeInDays" bson:"leadTimeInDays"`
-	unitCost          float64   `json:"unitCost" bson:"unitCost"`
-	criticalityLevel  int       `json:"criticalityLevel" bson:"criticalityLevel"`
+	ID                uuid.UUID `json:"id" bson:"_id"`
+	Name              string    `json:"name" bson:"name"`
+	Category          string    `json:"category" bson:"category"`
+	CurrentStock      int       `json:"currentStock" bson:"currentStock"`
+	MinimumStock      int       `json:"minimumStock" bson:"minimumStock"`
+	AverageDailySales float64   `json:"averageDailySales" bson:"averageDailySales"`
+	LeadTimeInDays    int       `json:"leadTimeInDays" bson:"leadTimeInDays"`
+	UnitCost          float64   `json:"unitCost" bson:"unitCost"`
+	CriticalityLevel  int       `json:"criticalityLevel" bson:"criticalityLevel"`
 }
---
+++ internal/repository/mongodb/gear_repository.go
@@ -0,0 +1,109 @@
+package mongodb
+
+import (
+	"context"
+
+	"gear-priority-api/internal/domain"
+
+	"github.com/google/uuid"
+
+	"go.mongodb.org/mongo-driver/v2/bson"
+	"go.mongodb.org/mongo-driver/v2/mongo"
+)
+
+type MongoGearRepository struct {
+	collection *mongo.Collection
+}
+
+func NewGearRepository(client *Client) *MongoGearRepository {
+	return &MongoGearRepository{
+		collection: client.DB.Collection("gears"),
+	}
+}
+
+func (r *MongoGearRepository) Create(ctx context.Context, gear *domain.Gear) error {
+	if gear.ID == uuid.Nil {
+		gear.ID = uuid.New()
+	}
+
+	_, err := r.collection.InsertOne(ctx, gear)
+
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (r *MongoGearRepository) FindAll(ctx context.Context) ([]domain.Gear, error) {
+
+	cursor, err := r.collection.Find(ctx, bson.M{})
+
+	if err != nil {
+		return nil, err
+	}
+
+	defer cursor.Close(ctx)
+
+	var gears []domain.Gear
+
+	if err := cursor.All(ctx, &gears); err != nil {
+		return nil, err
+	}
+
+	return gears, nil
+}
+
+func (r *MongoGearRepository) GetByID(ctx context.Context, id uuid.UUID) (*domain.Gear, error) {
+
+	var gear domain.Gear
+
+	err := r.collection.
+		FindOne(ctx, bson.M{
+			"_id": id,
+		}).
+		Decode(&gear)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return &gear, nil
+}
+
+func (r *MongoGearRepository) Update(ctx context.Context, gear *domain.Gear) error {
+
+	_, err := r.collection.UpdateByID(
+		ctx,
+		gear.ID,
+		bson.M{
+			"$set": gear,
+		},
+	)
+
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (r *MongoGearRepository) Delete(
+	ctx context.Context,
+	id uuid.UUID,
+) error {
+	_, err := r.collection.DeleteOne(
+		ctx,
+		bson.M{
+			"_id": id,
+		},
+	)
+
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// var _ domain.MongoGearRepository = (*MongoGearRepository)(nil)
